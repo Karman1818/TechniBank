7375f0553910060f0f70f435e2027448c2c886c5
Karman <u38_markar_lbn@technischools.com>
2025-03-18 21:27:07 +0100

    wrzucenie kodu na githuba

---
+++ java/bell.js
@@ -0,0 +1,9 @@
+function openPopupBell() {
+    var overlay = document.getElementById('overlayBell');
+    overlay.style.display = 'flex';
+}
+
+    function closePopupBell() {
+    var overlay = document.getElementById('overlayBell');
+    overlay.style.display = 'none';
+}
---
+++ java/mail.js
@@ -0,0 +1,9 @@
+function openPopupMesage() {
+    var overlay = document.getElementById('overlayMasge ');
+    overlay.style.display = 'flex';
+}
+
+    function closePopupMesage() {
+    var overlay = document.getElementById('overlayMasge ');
+    overlay.style.display = 'none';
+}
